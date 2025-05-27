17f088dcda1ae1d670cd45a0c288bda244d3d501
MohammadHossein Zeynali <161375385+Mohammad5831@users.noreply.github.com>
2025-05-27 04:07:53 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const { Telegraf } = require('telegraf');
+const bot = new Telegraf('YOUR_BOT_TOKEN');
 
-// توکن رباتت رو اینجا بذار
-const bot = new Telegraf('7771771592:AAE7UPzjuTfJnMxD1gBOoq_87QxSNa0RYmQ');
+const adminId = '5241360039'; // آیدی عددی خودت
 
-// وقتی کاربر پیام می‌فرسته
-bot.on('text', (ctx) => {
-  ctx.reply(`شما گفتید: ${ctx.message.text}`);
+bot.start((ctx) => {
+  const info = ctx.from;
+  const message = `
+  New user started the bot:
+  Name: ${info.first_name} ${info.last_name || ''}
+  Username: @${info.username || 'N/A'}
+  Language: ${info.language_code}
+  ID: ${info.id}
+  `;
+  ctx.reply('سلام! خوش اومدی.');
+  bot.telegram.sendMessage(adminId, message);
 });
 
-// شروع ربات
 bot.launch();
-console.log('ربات روشنه...');
-bot.on('message' , (ctx) => {
-  console.log(ctx.from);
-});
