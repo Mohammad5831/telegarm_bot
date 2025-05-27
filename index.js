3bd9b117936a788c4c65708ca42404651bc14349
MohammadHossein Zeynali <161375385+Mohammad5831@users.noreply.github.com>
2025-05-27 14:10:33 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const { Telegraf } = require('telegraf');
 const BOT_TOKEN = '7771771592:AAE7UPzjuTfJnMxD1gBOoq_87QxSNa0RYmQ'
 
 const axios = require('axios');
-const bot = new Telegraf(BOT_TOKEN, { poling: true} );
+const bot = new Telegraf(BOT_TOKEN, { polling: true} );
 
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    // bot.sendMessage(chatId, `سلام ${msg.from.first_name}! این یک پاسخ آزمایشی است.`);
-  bot.sendMessage(`test: ${msg} `)
+// bot.on('message', (msg) => {
+//     const chatId = msg.chat.id;
+//     // bot.sendMessage(chatId, `سلام ${msg.from.first_name}! این یک پاسخ آزمایشی است.`);
+//   bot.sendMessage(`test: ${msg} `)
+// });
+bot.on('text', (ctx) => {
+    ctx.reply(`سلام ${ctx.message.from.first_name}! این یک پاسخ آزمایشی است.`);
 });
 
 console.log("ربات فعال شد!");
